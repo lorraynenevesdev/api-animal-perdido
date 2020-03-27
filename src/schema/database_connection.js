f00e75dbbdbb63e1875342b94e9e89ff154442ac
Lorrayne Neves <lorrayneneves23@gmail.com>
2020-03-27 15:07:44 -0300

    Trocanco banco de dados

--- src/schema/database_connection.js
+++ src/schema/database_connection.js
@@ -1,12 +1,19 @@
 const sequelize_instance = require('sequelize'); 
-const host = 'mysql669.umbler.com';
-const port = 41890;
-const user = 'erico';
-const password = '92444070eol';
-const database = 'angular_nodejs';
+const host = 'ec2-52-200-119-0.compute-1.amazonaws.com';
+const port = 5432;
+const user = 'omdihiwudnswku';
+const password = '790f2a36363d30463c6a6a1a1422bb9530f91a873635a36140b6ca04f1577790';
+const database = 'd3h1e6gli70da7';
+
+// const sequelize_instance = require('sequelize'); 
+// const host = 'mysql669.umbler.com';
+// const port = 41890;
+// const user = 'erico';
+// const password = '92444070eol';
+// const database = 'angular_nodejs';
 
 const sequelize = new sequelize_instance(
-    `mysql://${user}:${password}@${host}:${port}/${database}`,{
+    `postgres://${user}:${password}@${host}:${port}/${database}`,{
         operatorsAliases : sequelize_instance.Op
     }
 )
@@ -14,7 +21,7 @@ const sequelize = new sequelize_instance(
 sequelize
     .authenticate()
     .then( ( ) => console.log( "OK" ) )
-    .catch( ( ) => console.log( "ERRO" ) )
+    .catch( ( error ) => console.log( "ERRO", error ) )
 
 //sequelize.sync()
 
